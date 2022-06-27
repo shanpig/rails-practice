a69ccdbd56e1c576455c0516d7eac807076a416d
shanpig <shanpigliao@gmail.com>
2022-06-27 22:26:44 +0800

    feat: associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   validates_presence_of :name
+  has_many :attendees
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_25_004352) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_27_141132) do
+  create_table "attendees", force: :cascade do |t|
+    t.string "name"
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "engineers", force: :cascade do |t|
     t.string "name"
     t.integer "age"
@@ -27,6 +41,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_25_004352) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_events_on_category_id"
   end
 
 end
